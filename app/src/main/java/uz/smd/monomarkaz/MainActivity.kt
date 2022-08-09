3315038501906aef9dce6bfd93582ea39484a209
Mukhriddin Siddikov <smdplayer1999@gmail.com>
2022-08-09 09:28:19 +0500

    Initial commit

---
+++ app/src/main/java/uz/smd/monomarkaz/MainActivity.kt
@@ -0,0 +1,18 @@
+package uz.smd.monomarkaz
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+      val wb=  findViewById<WebView>(R.id.wbView)
+        wb.settings.javaScriptEnabled=true
+        wb.setBackgroundColor(Color.TRANSPARENT);
+        wb.loadUrl("https://monomarkaz.uz/")
+    }
+}
